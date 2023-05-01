443d9ea5123ff9c060f0d97d6f02f2d52f4dc49a
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-05-01 01:55:04 -0500

    add back layout auth

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,43 +1,45 @@
 <template>
-  <img
-    src="https://res.cloudinary.com/dwmznusxf/image/upload/v1682826732/ngrg4put4ogyaaiikny1.png"
-    alt=""
-    sizes=""
-    srcset=""
-  />
-  <span class="text-3xl text-white font-bold"> ひさしぶり!</span>
-  <span class="text-sm text-white"> ¡Hola, te extrañamos!</span>
-  <div class="max-w-xs">
-    <form @submit.prevent="submitForm">
-      <div>
-        <label class="text-sm text-white" for="email">email</label>
-        <input
-          type="email"
-          id="email"
-          v-model="email"
-          class="border-2 border-white rounded-lg w-full p-2 bg-white bg-opacity-70"
-        />
-      </div>
-      <div>
-        <label class="text-sm text-white" for="password">Contraseña:</label>
-        <input
-          type="password"
-          id="password"
-          v-model="password"
-          class="border-2 border-white rounded-lg w-full p-2 bg-white bg-opacity-70"
-        />
-      </div>
-      <span class="text-sm text-fuchsia-200 text-right block mt-2"
-        >¿Olvido la contraseña?</span
-      >
-      <button
-        type="submit"
-        class="w-full mt-4 rounded-lg border-1 border-fuchsia-600 text-fuchsia-600 bg-fuchsia-200 p-3 text-sm"
-      >
-        Acceder
-      </button>
-    </form>
-    <SocialBtn islogin></SocialBtn>
+  <div class="flex flex-col items-center">
+    <img
+      src="https://res.cloudinary.com/dwmznusxf/image/upload/v1682826732/ngrg4put4ogyaaiikny1.png"
+      alt=""
+      sizes=""
+      srcset=""
+    />
+    <span class="text-3xl text-white font-bold"> ひさしぶり!</span>
+    <span class="text-sm text-white"> ¡Hola, te extrañamos!</span>
+    <div class="max-w-xs mt-4">
+      <form @submit.prevent="submitForm">
+        <div>
+          <label class="text-sm text-white" for="email">email</label>
+          <input
+            type="email"
+            id="email"
+            v-model="email"
+            class="border-2 border-white rounded-lg w-full p-2 bg-white bg-opacity-70"
+          />
+        </div>
+        <div>
+          <label class="text-sm text-white" for="password">Contraseña:</label>
+          <input
+            type="password"
+            id="password"
+            v-model="password"
+            class="border-2 border-white rounded-lg w-full p-2 bg-white bg-opacity-70"
+          />
+        </div>
+        <span class="text-sm text-fuchsia-200 text-right block mt-2"
+          >¿Olvido la contraseña?</span
+        >
+        <button
+          type="submit"
+          class="w-full mt-4 rounded-lg border-1 border-fuchsia-600 text-fuchsia-600 bg-fuchsia-200 p-3 text-sm"
+        >
+          Acceder
+        </button>
+      </form>
+      <SocialBtn islogin></SocialBtn>
+    </div>
   </div>
 </template>
 
