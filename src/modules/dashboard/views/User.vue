83e246c4f9ca7d01c3dc84b09343e63d51aed5da
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-05-01 01:41:20 -0500

    fix btn user

--- src/modules/dashboard/views/User.vue
+++ src/modules/dashboard/views/User.vue
@@ -6,20 +6,20 @@
 
     <div class="w-2/3">
       <span class="text-white">Selecciona una opción:</span>
-      <button
-        class="w-full mt-4 rounded-lg border-1 border-fuchsia-600 text-fuchsia-600 bg-fuchsia-200 p-3 font-bold"
-      >
-        <router-link :to="{ name: 'categories', params: { id: 0 } }">
+      <router-link :to="{ name: 'categories', params: { id: 0 } }">
+        <button
+          class="w-full mt-4 rounded-lg border-1 border-fuchsia-600 text-fuchsia-600 bg-fuchsia-200 p-3 font-bold"
+        >
           Ver todas las categorías
-        </router-link>
-      </button>
-      <button
-        class="w-full mt-4 rounded-lg border-1 border-fuchsia-600 text-fuchsia-600 bg-fuchsia-200 p-3 font-bold"
-      >
-        <router-link :to="{ name: 'create', params: { id: 0 } }">
+        </button>
+      </router-link>
+      <router-link :to="{ name: 'create', params: { id: 0 } }">
+        <button
+          class="w-full mt-4 rounded-lg border-1 border-fuchsia-600 text-fuchsia-600 bg-fuchsia-200 p-3 font-bold"
+        >
           Crear categorías
-        </router-link>
-      </button>
+        </button>
+      </router-link>
       <button
         class="w-full mt-4 rounded-lg border-1 border-fuchsia-600 text-fuchsia-600 bg-fuchsia-200 p-3 font-bold"
         @click="onLogout"
