2494a2f7c87be71f57e7a942b76fc140b7a348cc
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-05-01 02:18:51 -0500

    responsive categories

--- src/modules/dashboard/views/Categories.vue
+++ src/modules/dashboard/views/Categories.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex flex-col h-full py-10 ps-5">
     <span class="text-4xl text-white">Lista de Categorías</span>
-    <div class="flex flex-wrap gap-x-16 gap-y-10 mt-10 overflow-y-auto">
+    <div
+      class="flex flex-wrap gap-x-16 gap-y-10 mt-10 overflow-y-auto justify-center"
+    >
       <template v-for="item in listCategories" :key="item.id">
         <div
           class="w-72 h-40 bg-fuchsia-600 rounded-sm flex flex-col justify-center items-center p-3"
