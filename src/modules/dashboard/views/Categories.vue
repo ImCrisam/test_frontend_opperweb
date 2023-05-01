f34024471e097456ba79b8742e51b49ad7ccb1b9
Mauricio Arias <50003142+ImCrisam@users.noreply.github.com>
2023-05-01 04:31:14 -0500

    fix responsive categories

--- src/modules/dashboard/views/Categories.vue
+++ src/modules/dashboard/views/Categories.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="flex flex-col h-full py-10 ps-5">
-    <span class="text-4xl text-white">Lista de Categorías</span>
+  <div class="flex flex-col h-full md:py-10 px-5 py-5">
+    <span class="md:text-4xl text-2xl text-white">Lista de Categorías</span>
     <div
       class="flex flex-wrap gap-x-16 gap-y-10 mt-10 overflow-y-auto justify-center"
     >
       <template v-for="item in listCategories" :key="item.id">
         <div
-          class="w-72 h-40 bg-fuchsia-600 rounded-sm flex flex-col justify-center items-center p-3"
+          class="md:w-72 md:h-40 w-52 h-32 bg-fuchsia-600 rounded-sm flex flex-col justify-center items-center p-3"
         >
-          <span class="text-4xl text-white mt-auto px-5">{{
+          <span class="md:text-4xl text-2xl text-white mt-auto px-5">{{
             item.nombre
           }}</span>
           <button
